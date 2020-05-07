72409fe9cde609def249eba050de3906935502b9
SoulChild <jinhui.tang@maiunsoft.com>
2020-05-07 12:14:25 +0800

    增加Myprint方法，支持打印颜色

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,6 +1,19 @@
 package org.devops
 
-//打包内容
+// 打印内容
 def PrintMes(content) {
   print(content)
+}
+
+// 带颜色打印
+def Myprint(content,color) {
+  colors = [
+    'red': "\033[40;31m ${content} \033[0m",
+    'green':  "[1;32m ${content} [m",
+    'bule': "\033[47;34m ${content} \033[0m",
+    'green1': "\033[40;32m ${content} \033[0m"
+  ]
+  ansiColor('xterm') {
+    print(colors[${color}])
+  }
 }
