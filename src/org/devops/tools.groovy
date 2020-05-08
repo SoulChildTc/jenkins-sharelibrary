1e7c8b520ba3141a5e364024081e374cb1d4c861
SoulChild <jinhui.tang@maiunsoft.com>
2020-05-08 10:14:22 +0800

    add build func

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -17,3 +17,13 @@ def Myprint(content,color) {
     println(colors[${color}])
   }
 }
+
+// 构建类型
+def build(buildType,buildShell,){
+  def buildTools = ['mvn': 'M2', 'npm':'NPM']
+
+  println("当前选择的构建类型为:${buildType}")
+  buildHome = tool buildTools[buildType]
+  
+  sh "${buildHome}/bin/${buildType} ${buildShell}"
+}
